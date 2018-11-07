03136158a8aef11891fd6d9cb6887df3814d480b
Carl Regårdh <calleisking@gnail.com>
2018-11-07 14:24:47 +0100

    Added svg-pictures as icons (Nina, William, Oliver)

---
+++ media/Quire_files/tutorial.js
@@ -0,0 +1,34 @@
+let tutorial = document.querySelector(".tutorial");
+tutorial.addEventListener("click", ()=>{
+  tutorial.style.display = "none";
+})
+var key_value = "beenhere";
+   var foundCookie = 0;
+
+   // Get all the cookies from this site and store in an array
+   var cookieArray = document.cookie.split(';');
+
+       // Walk through the array
+       for(var i=0;i < cookieArray.length;i++)
+           {
+                  var checkCookie = cookieArray[i];
+           // Remove any leading spaces
+                  while (checkCookie.charAt(0)==' ')
+                  {
+                    checkCookie = checkCookie.substring(1,checkCookie.length);
+                  }
+
+           // Look for cookie set by key_value
+                   if (checkCookie.indexOf(key_value) == 0)
+                  {
+               // The cookie was found so set the variable
+                      foundCookie = 1;
+                  }
+       }
+       // Check if a cookie has been found
+       if ( foundCookie == 0)
+       {
+           // The key_value cookie was not found so set it now
+          document.cookie = key_value;
+          tutorial.style.display= "block";
+       }
