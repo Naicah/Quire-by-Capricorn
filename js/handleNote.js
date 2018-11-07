3962470ceb3431333df20504d7e9528ce0d90eb4
willmister <wnordqvist@gmail.com>
2018-11-07 19:13:32 +0100

    moved nav js to a separate js file

--- js/handleNote.js
+++ js/handleNote.js
@@ -56,37 +56,6 @@ function c () {
 }
 
 
-// Toggle hamburger to toolbar in mobile version / William
 
-var noteList = document.getElementById("noteList");
-var iconimages = document.querySelector(".iconimages");
-document.getElementById("hamburgerIcon").addEventListener("click",function(){
-    if(iconimages.style.display === 'none'){
-        iconimages.style.display = 'flex'
-       } else {
-		iconimages.style.display = 'none'
-	   }});
-
-// Toggle Notelist overwiew in mobile version / William	   
-
-document.getElementById("folder").addEventListener("click",function(){
-if(noteList.style.display === 'none'){
-	noteList.style.display = 'block'
-	} else {
-	noteList.style.display = 'none'
-	}});
-
-// adjust elements between phone and computer displey / William
-	   
-window.addEventListener("resize", function() {
-	if (window.matchMedia("(min-width: 768px)").matches) {
-		iconimages.style.display = 'flex';
-		noteList.style.display = 'block';
-	
-	} else {
-		iconimages.style.display = 'none';
-		noteList.style.display = 'none';
-	}
-});
 
 
