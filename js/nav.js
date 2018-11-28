2cc89fb2814055d972c440c2fa71bd87dfc27293
jonathans <jonathan.silkewall@gmail.com>
2018-11-28 15:02:28 +0100

    fixed js/ fixed qltoolbar

--- js/nav.js
+++ js/nav.js
@@ -8,3 +8,24 @@ function toggleMenu() {
 function toggleNoteList() {
 	document.getElementById("noteList").classList.toggle('none');
 }
+
+
+
+//STICKY NAV MOBILE
+function setToolbarPositionStatus(){
+	let state = true;
+	let noteList = document.querySelector("#noteList");
+	let topNav = document.querySelector("#topNav");
+	let toolbar = document.querySelector(".ql-toolbar");
+	let distanceToBottomTopNav = topNav.getBoundingClientRect().bottom;
+	let distanceToBottomNoteList = noteList.getBoundingClientRect().bottom;
+	let distanceToTop = toolbar.getBoundingClientRect().top;
+	if(distanceToBottomTopNav >= 1 || distanceToBottomNoteList >= 1){
+		state = false;
+	}
+	if(distanceToTop <= 0  && state == true){
+		toolbar.classList.add("ql-toolbar-fixed")
+	}else{
+		toolbar.classList.remove("ql-toolbar-fixed")
+	}
+}
