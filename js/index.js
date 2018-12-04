50d91e64c12e63665c85a2480a01a82f6ade991b
nina hedman <ni.hedman@gmail.com>
2018-12-04 13:35:30 +0100

    Added close pop up via X or click outside pop up

--- js/index.js
+++ js/index.js
@@ -8,10 +8,79 @@ window.onload = function () {
     displayFirstNote(); // Display content of first note of note list in editor
     positionSaveButton(); //Move the save button to be within toolbar
 
-    // WHEN CLICK IN TUTORIAL POP UP
-    document.getElementById("tutorial").addEventListener("click", () => {
-        document.getElementById("tutorial").style.display = "none"; // Hide tutorial
+    //----------------------------  POP-UPS ------------------------------ //
+    // WHEN CLICK OUTSIDE A POP UP
+    document.getElementById("popUpBg").addEventListener("click", () => {
+        let popUp;
+        let tutorial = document.getElementById("tutorial");
+        let unsavedContent = document.getElementById("unsavedContent");
+        let chooseTheme = document.getElementById("chooseTheme");
+        if (!tutorial.classList.contains("none")) {
+            popUp = tutorial;
+        } else if (!unsavedContent.classList.contains("none")) {
+            popUp = unsavedContent;
+        } else if (!chooseTheme.classList.contains("none")){
+            popUp = chooseTheme;
+        }
+        popUpToggle(popUp);
     });
+    //---------------------  UNSAVED POP UP------------------- //
+        // WHEN CLICK ON SAVE IN UNSAVED POP UP
+    document.getElementById("unsavedContentSave").addEventListener("click", function () {
+        unsavedContentSave(getCurrentNoteID(), getNextNoteID()); // Save changes and display next note
+        popUpToggle(document.getElementById("unsavedContent")); // Hide warning pop up
+    });
+
+    // WHEN CLICK ON IGNORE IN UNSAVED POP UP
+    document.getElementById("unsavedContentIgnore").addEventListener("click", function () {
+        unsavedContentIgnore(getNextNoteID()); // Ignore changes and display next note
+        popUpToggle(document.getElementById("unsavedContent")); // Hide warning pop up
+    });
+
+    // WHEN CLICK ON CANCEL IN UNSAVED POP UP
+    document.getElementById("unsavedContentCancel").addEventListener("click", function () {
+        popUpToggle(document.getElementById("unsavedContent")); // Hide warning pop up
+    });
+    // WHEN CLICK ON X IN UNSAVED POP UP
+    document.getElementById('exitunsavedContent').addEventListener('click', function () {
+        popUpToggle(document.getElementById("unsavedContent")); // Hide warning pop up
+    });
+    //---------------------  TUTORIAL POP UP------------------- //
+    // WHEN CLICK ON X IN TUTORIAL POP UP
+    document.getElementById('tutorial').addEventListener('click', function () {
+        popUpToggle(document.getElementById("tutorial")); // Hide warning pop up
+    });
+
+    //---------------------  THEME POP UP------------------- //
+    // WHEN CLICKING ON THEME ICON
+    document.getElementById("themeIcon").addEventListener("click", function () {
+        popUpToggle(document.getElementById("chooseTheme"));
+    });
+
+    // WHEN CLICK ON FORREST THEME
+    document.getElementById("forrest").addEventListener("click", function () {
+        changeTheme("forrest"); // Change theme
+    });
+    // WHEN CLICK ON FIRE THEMW
+    document.getElementById("fire").addEventListener("click", function () {
+        changeTheme("fire"); // Change theme
+    });
+
+    // WHEN CLICK ON WATER THEME
+    document.getElementById("water").addEventListener("click", function () {
+        changeTheme("water"); // Change theme
+    });
+    // WHEN CLICK ON STANDARD THEME
+    document.getElementById("standard").addEventListener("click", function () {
+        changeTheme("standard"); // Change theme
+    });
+
+    // WHEN CLICK ON X IN CHOOSE THEME POP UP
+     document.getElementById('chooseTheme').addEventListener('click', function () {
+        popUpToggle(document.getElementById("chooseTheme")); // Hide warning pop up
+    });
+    
+    //---------------------------- Slut pop-ups --------------------------- //
 
     // WHEN CLICK ON HAMBURGER ICON IN MOBILE LAYOUT
     document.getElementById("hamburgerIcon").addEventListener("click", function () {
@@ -48,50 +117,6 @@ window.onload = function () {
         document.getElementById("searchField").firstElementChild.classList.toggle('textarea');
     });
 
-    // WHEN CLICK ON SAVE IN POP UP
-    document.getElementById("unsavedContentSave").addEventListener("click", function () {
-        unsavedContentSave(getCurrentNoteID(), getNextNoteID()); // Save changes and display next note
-        document.getElementById("unsavedContent").classList.toggle('none'); // Hide warning pop up
-    });
-
-    // WHEN CLICK ON IGNORE IN POP UP
-    document.getElementById("unsavedContentIgnore").addEventListener("click", function () {
-        unsavedContentIgnore(getNextNoteID()); // Ignore changes and display next note
-        document.getElementById("unsavedContent").classList.toggle('none'); // Hide warning pop up
-    });
-
-    // WHEN CLICK ON IGNORE IN POP UP
-     document.getElementById("unsavedContentCancel").addEventListener("click", function () {
-        document.getElementById("unsavedContent").classList.toggle('none'); // Hide warning pop up
-    });
-
-    // WHEN CLICKING ON THEME ICON
-    document.getElementById("themeIcon").addEventListener("click", function () {
-        themeToggle(); // Show pop up to choose theme
-    });
-
-    // WHEN CLICKING OUTSIDE POP UP TO CHOOSE THEME
-    document.getElementById("chooseTheme").addEventListener("click", function () {
-        themeToggle(); // Hide pop up to choose theme
-    });
-
-    // WHEN CLICK ON FORREST THEME
-    document.getElementById("forrest").addEventListener("click", function () {
-        changeTheme("forrest"); // Change theme
-    });
-    // WHEN CLICK ON FIRE THEMW
-    document.getElementById("fire").addEventListener("click", function () {
-        changeTheme("fire"); // Change theme
-    });
-
-    // WHEN CLICK ON WATER THEME
-    document.getElementById("water").addEventListener("click", function () {
-        changeTheme("water"); // Change theme
-    });
-    // WHEN CLICK ON STANDARD THEME
-    document.getElementById("standard").addEventListener("click", function () {
-        changeTheme("standard"); // Change theme
-    });
 
     // SHOW FAVORITES
     document.getElementById("favIcon").addEventListener("click", function () {
